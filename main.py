00a4a275f5ce0810eb04717b74b2f572e8576842
Roman <92420080+RRoman202@users.noreply.github.com>
2024-02-08 17:33:38 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import numpy as np
 import pandas as pd
 import pickle
-import uuid
-from fastapi import FastAPI, Body, status, Request
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi import FastAPI, status, Request
+from fastapi.responses import JSONResponse
 from starlette.staticfiles import StaticFiles
-from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
+import joblib
 
 app = FastAPI()
 templates = Jinja2Templates(directory="public")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 df = pd.read_csv('data.csv')
-similarity_content_matrix = np.load('matrix_content_rating.npz')['arr']
-similarity_genre_matrix = np.load('matrix_genres_rating.npz')['arr']
+
+with open('tf_content.pkl', 'rb') as file:
+    tf_content = pickle.load(file)
+
+with open('tf_genre.pkl', 'rb') as file:
+    tf_genre = pickle.load(file)
+
 pop = pd.read_csv('pop_film.csv')
+
+model_content = joblib.load('model_content.pkl')
+model_genre = joblib.load('model_genre.pkl')
 
 genres = ['Animation', 'Comedy', 'Family', 'Adventure', 'Fantasy', 'Romance', 'Drama', 'Action', 'Crime', 'Thriller', 'Horror', 'History', 'Science Fiction', 'Mystery', 'War', 'Foreign', 'Music', 'Documentary', 'Western', 'TV Movie']
 
@@ -24,41 +31,16 @@ def get_top10():
     pop['w_score'] = pop['w_score'].round(2)
     return pop[['title','w_score','release_date']]
 
-def get_content_recommendations(movie_title, similarity_matrix, n=10):
+
+def get_content_recommendations(movie_title, tfidf_matrix, model):
     movie_index = df[df['title'] == movie_title].index[0]
+    movie_tfidf = tfidf_matrix[movie_index]
+    _, indices = model.kneighbors(movie_tfidf, n_neighbors=100)
 
-    similar_movies = list(enumerate(similarity_matrix[movie_index]))
+    recommended_movies = df[['title', 'w_score', 'release_date']].iloc[indices[0][1:]].drop_duplicates()
+    recommended_movies = recommended_movies[recommended_movies['title'] != movie_title].head(10)
 
-    similar_movies_sorted = sorted(similar_movies, key=lambda x: x[1], reverse=True)
-
-    recommended_movies = []
-    recommended_rating = []
-    recommended_release = []
-    recommended_count = 0
-
-    for index, similarity in similar_movies_sorted:
-        if index != movie_index:
-            recommended_movie_title = df.iloc[index]['title']
-            recommended_movie_rating = df.iloc[index]['w_score']
-            recommended_movie_release_date = df.iloc[index]['release_date']
-
-            if recommended_movie_title != movie_title:
-                if recommended_movie_title not in recommended_movies:
-                    recommended_movies.append(recommended_movie_title)
-                    recommended_rating.append(recommended_movie_rating)
-                    recommended_release.append(recommended_movie_release_date)
-                    recommended_count += 1
-
-                    if recommended_count == n:
-                        break
-
-    films = []
-    for i in range(len(recommended_movies)):
-        films.append((recommended_movies[i], recommended_rating[i], recommended_release[i]))
-
-    recommended_df = pd.DataFrame(films, columns=['title', 'rating', 'release_date'])
-    recommended_df['rating'] = recommended_df['rating'].round(2)
-    return recommended_df
+    return recommended_movies
 
 
 def get_genre_recommendation(genre_name):
@@ -68,7 +50,7 @@ def get_genre_recommendation(genre_name):
     genre_movies = genre_movies.drop_duplicates(subset='title')
     top_genre_movies = genre_movies.sort_values(by="w_score", ascending=False).head(10)
     top_genre_movies['w_score'] = top_genre_movies['w_score'].round(2)
-    return top_genre_movies[['title', 'w_score', 'release_date']]Ё
+    return top_genre_movies[['title', 'w_score', 'release_date']]
 
 @app.get("/")
 async def root(request: Request):
@@ -97,7 +79,7 @@ async def genre(request: Request):
 async def content_recommendation(request: Request, film_name: str):
     if film_name not in df['title'].values:
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"error": "Фильм не существует"})
-    films = get_content_recommendations(film_name, similarity_content_matrix)
+    films = get_content_recommendations(film_name, tf_content, model_content)
     films = films.values.tolist()
     return templates.TemplateResponse("rec_content.html", {"request": request, "films": films, "film_content": film_name})
 
@@ -106,7 +88,7 @@ async def content_recommendation(request: Request, film_name: str):
 async def genre2_recommendation(request: Request, film_name: str):
     if film_name not in df['title'].values:
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"error": "Фильм не существует"})
-    films = get_content_recommendations(film_name, similarity_genre_matrix)
+    films = get_content_recommendations(film_name, tf_genre, model_genre)
     films = films.values.tolist()
     return templates.TemplateResponse("rec_genre.html", {"request": request, "films": films, "genres": genres, "film_genre": film_name})
 
